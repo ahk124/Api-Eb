ce55f2c94c9187426a4abcba74229bf4dbe44eae
ahk124 <ahk0212@gmail.com>
2021-03-08 23:52:57 +0330

    api result filter

--- ApiProject/Controllers/UserController.cs
+++ ApiProject/Controllers/UserController.cs
@@ -8,10 +8,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Services.Repository;
 using WebFramework.Api;
+using WebFramework.Filters;
 
 namespace ApiProject.Controllers
 {
     [Route("api/[controller]")]
+    [ApiResultFilter]
     [ApiController]
     public class UserController : ControllerBase
     {
@@ -19,37 +21,31 @@ namespace ApiProject.Controllers
         public UserController(IUserRepository userRepository)
         {
             this._userRepository = userRepository;
-
         }
+
         [HttpGet]
         public async Task<ApiResult<List<UserModel>>> Get(CancellationToken cancellationToken)
         {
             var users = await _userRepository.TableNoTracking.ToListAsync(cancellationToken);
-            return users;
+            if (users == null)
+                NotFound();
+            return Ok(users);
         }
 
         [HttpGet("{id:int}")]
         public async Task<ApiResult<UserModel>> Get(int id, CancellationToken cancellationToken)
         {
             var user = await _userRepository.GetByIdAsync(cancellationToken, id);
-            return new ApiResult<UserModel>
-            (true,
-                 ApiResultStatusCode.Success,
-                  user,
-                 "عملیات با موفقیت انجام شد"
-            );
+            if (user == null)
+                NotFound();
+            return Ok(user);
         }
 
         [HttpPost]
         public async Task<ApiResult> Create([FromBody] UserModel user, CancellationToken cancellationToken)
         {
             await _userRepository.AddAsync(user, cancellationToken);
-            return new ApiResult<UserModel>
-            {
-                IsSuccess = true,
-                StatusCode = ApiResultStatusCode.Success,
-                Message = "عملیات با موفقیت انجام شد",
-            };
+            return Ok();
         }
 
         [HttpPut("{id}")]
@@ -63,13 +59,7 @@ namespace ApiProject.Controllers
             userUpdate.IsActive = user.IsActive;
             userUpdate.LastLoginDate = user.LastLoginDate;
             await _userRepository.UpdateAsync(userUpdate, cancellationToken);
-            return new ApiResult<UserModel>
-            {
-                IsSuccess = true,
-                StatusCode = ApiResultStatusCode.Success,
-                Message = "عملیات با موفقیت انجام شد",
-                Data = user
-            };
+            return Ok();
         }
 
         [HttpDelete("{id}")]
@@ -77,12 +67,7 @@ namespace ApiProject.Controllers
         {
             var userDelete = await _userRepository.GetByIdAsync(cancellationToken, id);
             await _userRepository.DeleteAsync(userDelete, cancellationToken);
-            return new ApiResult<UserModel>
-            {
-                IsSuccess = true,
-                StatusCode = ApiResultStatusCode.Success,
-                Message = "عملیات با موفقیت انجام شد",
-            };
+            return Ok();
         }
     }
 }
